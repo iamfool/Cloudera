e6f1326080e36effd5bfba38e6cdb53e76ad0aca
heibalvin <heibalvin@yahoo.com>
2016-12-19 11:50:47 +0100

    updated jdk version to 8u112

--- director-quick-install-snippet-centos-6.5.sh
+++ director-quick-install-snippet-centos-6.5.sh
@@ -5,8 +5,8 @@ yum install -y wget
 wget --directory-prefix=/etc/yum.repos.d/ http://archive.cloudera.com/director/redhat/6/x86_64/director/cloudera-director.repo
 yum update -y
 
-wget --no-check-certificate --no-cookies --header 'Cookie: oraclelicense=accept-securebackup-cookie' http://download.oracle.com/otn-pub/java/jdk/8u102-b14/jdk-8u102-linux-x64.rpm
-yum localinstall -y jdk-8u102-linux-x64.rpm
+sudo wget --no-check-certificate --no-cookies --header 'Cookie: oraclelicense=accept-securebackup-cookie' http://download.oracle.com/otn-pub/java/jdk/8u112-b15/jdk-8u112-linux-x64.rpm
+sudo yum localinstall -y jdk-8u112-linux-x64.rpm
 
 yum install -y cloudera-director-server cloudera-director-client
 service cloudera-director-server start
